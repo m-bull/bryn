0bee9e6186122f76279dfefab56fc9132e84a718
Nick Loman <n.j.loman@bham.ac.uk>
2017-02-23 08:30:46 +0000

    hackathon volume code in new branch

--- brynweb/home/urls.py
+++ brynweb/home/urls.py
@@ -13,6 +13,12 @@ urlpatterns = [
     url(r'^terminate/(?P<teamid>\d+)/(?P<uuid>[^/]+)$', views.terminate, name='terminate'),
     url(r'^shelve/(?P<teamid>\d+)/(?P<uuid>[^/]+)$', views.shelve, name='shelve'),
     url(r'^unshelve/(?P<teamid>\d+)/(?P<uuid>[^/]+)$', views.unshelve, name='unshelve'),
+    ## volumes
+    url(r'^vol-connect/(?P<teamid>\d+)/(?P<uuid>[^/]+)$', views.vol_connect, name='vol-connect'),
+    url(r'^vol-disconnect/(?P<teamid>\d+)/(?P<uuid>[^/]+)$', views.vol_disconnect, name='vol-disconnect'),
+    url(r'^vol-snapshot/(?P<teamid>\d+)/(?P<uuid>[^/]+)$', views.vol_snapshot, name='vol-snapshot'),
+    url(r'^vol-delete/(?P<teamid>\d+)/(?P<uuid>[^/]+)$', views.vol_delete, name='vol-delete'),
+
     url(r'^get_instances_table', views.get_instances_table, name='get_instances_table'),
     url(r'^$', views.home, name='home'),
 ]
